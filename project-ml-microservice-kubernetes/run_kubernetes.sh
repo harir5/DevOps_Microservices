be7158461ed292bf084d47b44ca1a1b81a9a0db5
EC2 Default User <ec2-user@ip-172-31-85-154.ec2.internal>
2022-06-11 01:05:49 +0000

    master : committing changes for project

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=harihr5/udacityp4repo
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+kubectl get nodes
+kubectl run udacityproject4 --image=harihr5/udacityp4repo:udacityproject4 --port=8000
+kubectl wait --for=condition=Ready=true pod/udacityproject4
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/udacityproject4 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,17 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=harihr5/udacityp4repo
 
 # Step 2:  
 # Authenticate & tag
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 070874708673.dkr.ecr.us-east-1.amazonaws.com
+docker tag udacityproject4:latest 070874708673.dkr.ecr.us-east-1.amazonaws.com/udacityproject4:latest
+docker login -u harihr5
+docker tag udacityproject4 $dockerpath:udacityproject4
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push 070874708673.dkr.ecr.us-east-1.amazonaws.com/udacityproject4:latest
+docker push $dockerpath:udacityproject4
